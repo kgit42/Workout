748d15e615822230e26b1962f0116675e01c4060
kgit42 <49207690+kgit42@users.noreply.github.com>
2021-12-30 16:49:59 +0100

    .

--- app/src/main/java/com/example/workout/db/ExerciceDao.kt
+++ app/src/main/java/com/example/workout/db/ExerciceDao.kt
@@ -5,7 +5,7 @@ import androidx.room.*
 
 @Dao
 interface ExerciceDao {
-    @Query("SELECT * FROM exercice ORDER BY name")
+    @Query("SELECT * FROM exercice ORDER BY category, name")
     fun getAll(): LiveData<List<Exercice>>
 
     @Query("SELECT * FROM exercice WHERE eid IN (:exerciceIds)")
